fd99bd7155e8e5588a8dec89416d9a251bfd8556
kazuki19992 <kushida98@gmail.com>
2021-09-03 22:08:21 +0900

    名刺

---
+++ data/datas.js
@@ -0,0 +1,10 @@
+const datas = {
+  mail: "ceka18065@g.nihon-u.ac.jp",
+  twitter: "Tech_Kazu",
+  company: "日本大学 工学部 B4",
+  github: "kazuki19992",
+  birth: "1999年04月06日",
+  place: "福島県郡山市"
+}
+
+export default datas
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from 'next/head'
 
+import NameCard from '../components/index/NameCard'
+
 const Home = () => {
   return (
       <section className="h-screen w-4/5 max-w-5xl mx-auto flex items-center justify-center flex-col">
-        <h1 className="mb-4 text-indigo-900 text-3xl font-bold">かずのすけくんだよ</h1>
+        {/* <h1 className="mb-4 text-indigo-900 text-3xl font-bold">かずのすけくんだよ</h1>
+        <p className="">まだ工事中だよ</p> */}
+        <NameCard />
       </section>
   )
 }
