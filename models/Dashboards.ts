6abb17958efb0d0047f1ea0056d1961384ce3c80
kazuki19992 <kushida98@gmail.com>
2023-09-12 22:37:22 +0900

    use bun

--- models/Dashboards.ts
+++
@@ -1,7 +0,0 @@
-import * as fa from "@fortawesome/free-solid-svg-icons";
-export type CarrierElementProps = {
-  title: string;
-  subtitle: string;
-  icon: fa.IconDefinition;
-  isCurrent?: boolean;
-};
